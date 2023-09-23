c28a9d8aadc570f5e1746174620e32e8c2f33dac
JulianGomez <132085190+julian98789@users.noreply.github.com>
2023-09-23 14:34:46 -0500

    Creacion del proyecto
    Se a creado el proyecto llamado App_Hospital

---
+++ APP_Hospital/src/main/java/com/mycompany/app_hospital/APP_Hospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app_hospital;
+
+/**
+ *
+ * @author julian
+ */
+public class APP_Hospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
